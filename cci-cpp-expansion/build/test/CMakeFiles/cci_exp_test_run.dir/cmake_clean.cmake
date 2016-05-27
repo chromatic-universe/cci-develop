5adb2a6d7d12634a4192973ef2005e07f18d25fa
chromatic-universe <chromaticuniverse79@gmail.com>
2016-05-27 08:42:40 -0400

    cci_presents

---
+++ cci-cpp-expansion/build/test/CMakeFiles/cci_exp_test_run.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cci_exp_test_run.dir/test_cci_numeric_utils.cpp.o"
+  "cci_exp_test_run.pdb"
+  "cci_exp_test_run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cci_exp_test_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
