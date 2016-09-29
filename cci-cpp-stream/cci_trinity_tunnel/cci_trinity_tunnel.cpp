83e9657130fd7b50e0ef95be40edc4d6e79aea1d
chromatic-universe <chromaticuniverse79@gmail.com>
2016-09-28 20:13:48 -0400

    cpp trinity

--- cci-cpp-stream/cci_trinity_tunnel/cci_trinity_tunnel.cpp
+++ cci-cpp-stream/cci_trinity_tunnel/cci_trinity_tunnel.cpp
@@ -37,7 +37,18 @@ using namespace cpp_real_stream;
 //signals
 void sigterm( int sig )
 {
+    std::unique_ptr<time_utils> tu ( new time_utils( stamp_color::green ) );
+    tu->null_stamp();
+    std::cerr << "[trinity] ";
+    tu->time_stamp();
+    std::cerr << "..caught SIGTERM...\n";
+    std::cerr << "[trinity] ";
+    tu->time_stamp();
+    std::cerr << "..server shutdown...fini....\n";
+
     std::cerr << "\033[0m\n" ;
+
+
     exit( 0 );
 }
 
@@ -324,22 +335,21 @@ void dispatch_direct_tcp( const std::string& host_tuple ,
                   v_dest = util::split( dest_tuple , colon );
 
                   cci_trinity::trinity_acceptor acceptor( ios ,
-                                                          local_host ,
-                                                          local_port ,
-                                                          forward_host ,
-                                                          forward_port );
-
-                  acceptor.accept_connections();
-
+                                                          v_host[0],
+                                                          stoi( v_host[1] ) ,
+                                                          v_dest[0] ,
+                                                          stoi( v_dest[1] ) ) ;
                   std::unique_ptr<time_utils> tu ( new time_utils( stamp_color::green ) );
                   tu->null_stamp();
                   std::cerr << "[trinity] ";
-
+                  tu->time_stamp();
 
                   std::cerr << "..starting tcp proxy for "
                             << dest_tuple
-                            << "....\n"
+                            << "....\n";
                   tu->clear_color();
+
+                  acceptor.accept_connections();
 
                   ios.run();
            }
