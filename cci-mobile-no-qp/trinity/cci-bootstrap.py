d86c730efbde4b9d7f0370a9bc2eb1cfa940639e
chromatic-universe <chromaticuniverse79@gmail.com>
2016-10-06 09:09:11 -0400

    all

---
+++ cci-mobile-no-qp/trinity/cci-bootstrap.py
@@ -0,0 +1,23 @@
+import os
+import logging
+from time import sleep
+
+if __name__ == '__main__':
+    
+        log_format = '%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s'
+
+
+# logger
+        _logger = logging.getLogger( "cci-trinity-server" )
+        _logger.setLevel( logging.DEBUG )
+        fh = logging.FileHandler(  'cci-trinity-server.log' + '-debug.log', mode = 'a' )
+        fh.setLevel( logging.DEBUG )
+        formatter = logging.Formatter( log_format )
+        fh.setFormatter( formatter )
+        _logger.addHandler( fh )
+
+
+        while True :
+            _logger.info( 'the_original_corny_snaps!' )
+            sleep( 2 )
+		
