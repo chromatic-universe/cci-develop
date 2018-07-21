5b8ea0ada2ff39ff6ecc0f8f15b5f77ddd1e6cef
chromatic-universe <chromaticuniverse79@gmail.com>
2018-07-21 09:13:05 -0400

    stream event kafka composite

--- cci-cpp-stream/include/cci_stream_event.h
+++ cci-cpp-stream/include/cci_stream_event.h
@@ -27,6 +27,15 @@ namespace cpp_real_stream
                     sec_plugin ,
 					sec_untyped
 				};
+                //
+                enum class transport_mechanism : unsigned long
+				{
+					tm_direct_p_and_s =  0 ,
+                    tm_proxy_p_abd_s ,
+                    tm_tunnel_p_and_s ,
+					tm_http_post_to_queue
+				};
+
 
 
                 class cci_stream_event
@@ -37,11 +46,20 @@ namespace cpp_real_stream
                                 //ctors
                                 cci_stream_event();
                                 explicit cci_stream_event(  kafka_producer_t kpt ,
+                                                            transport_mechanism tm = transport_mechanism::tm_http_post_to_queue ,
                                                             stream_event_class sec = stream_event_class::sec_internal );
 
 
                                 //dtor
-                                ~cci_stream_event();
+                                ~cci_stream_event() = default;
+
+
+                                //no copy
+                                cci_stream_event( const cci_stream_event& ) = delete;
+                                cci_stream_event( const cci_stream_event&& ) = delete;
+                                //no assign
+                                cci_stream_event& operator= ( const cci_stream_event&  ) = delete;
+                                cci_stream_event& operator= ( const cci_stream_event&&  ) = delete;
 
 
                             private :
@@ -53,7 +71,9 @@ namespace cpp_real_stream
                             protected :
 
 
-                                //
+                                //sttrributes
+                                stream_event_class          m_event_class;
+                                transport_mechanism         m_transport;
 
 
                             public :
@@ -61,8 +81,12 @@ namespace cpp_real_stream
 
                                 //accessors-inspectiors
                                 kafka_producer_t  producer() { return m_producer; }
+                                stream_event_class event_class() const noexcept { return m_event_class; }
+                                transport_mechanism transport() const noexcept { return  m_transport; }
                                 //mutators
                                 void producer( kafka_producer_t kpt ) { m_producer = kpt; }
+                                void event_class( const stream_event_class& ec ) { m_event_class = ec; }
+                                void transport( const transport_mechanism& tm ) { m_transport = tm; }
 
                 };
 }
--- cci-cpp-stream/stream_event/src/cci_stream_event.cpp
+++ cci-cpp-stream/stream_event/src/cci_stream_event.cpp
@@ -17,7 +17,10 @@ cci_stream_event::cci_stream_event()
 
 //-----------------------------------------------------------------------------------------
 cci_stream_event::cci_stream_event( kafka_producer_t kpt ,
-                                    stream_event_class sec ) : m_producer { kpt }
+                                    transport_mechanism tm ,
+                                    stream_event_class sec ) : m_producer { kpt } ,
+                                                               m_event_class { sec } ,
+                                                               m_transport { tm }
 {
         assert( kpt );
 }
