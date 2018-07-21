3b90d156c7199dfaf0b84c876623362989fe35cf
chromatic-universe <chromaticuniverse79@gmail.com>
2018-07-20 23:44:07 -0400

    stream event

--- cci-cpp-stream/include/cci_stream_event.h
+++ cci-cpp-stream/include/cci_stream_event.h
@@ -14,8 +14,57 @@
 namespace cpp_real_stream
 {
 
+                //aliases
+                using kafka_producer_t = cci_kafka_producer*;
 
+
+                //enumerations
+                enum class stream_event_class : unsigned long
+				{
+					sec_external =  0 ,
+					sec_internal ,
+					sec_supplicant ,
+                    sec_plugin ,
+					sec_untyped
+				};
+
+
+                class cci_stream_event
+                {
+
+                            public :
+
+                                //ctors
+                                cci_stream_event();
+                                explicit cci_stream_event(  kafka_producer_t kpt ,
+                                                            stream_event_class sec = stream_event_class::sec_internal );
+
+
+                                //dtor
+                                ~cci_stream_event();
+
+
+                            private :
+
+                                //attributes
+                                kafka_producer_t            m_producer;
+
+
+                            protected :
+
+
+                                //
+
+
+                            public :
+
+
+                                //accessors-inspectiors
+                                kafka_producer_t  producer() { return m_producer; }
+                                //mutators
+                                void producer( kafka_producer_t kpt ) { m_producer = kpt; }
+
+                };
 }
-
 
 
--- cci-cpp-stream/stream_event/src/cci_stream_event.cpp
+++ cci-cpp-stream/stream_event/src/cci_stream_event.cpp
@@ -4,5 +4,24 @@
 #include <cci_stream_event.h>
 
 
+using namespace cpp_real_stream;
+
+
+
+
+//-----------------------------------------------------------------------------------------
+cci_stream_event::cci_stream_event()
+{
+}
+
+
+//-----------------------------------------------------------------------------------------
+cci_stream_event::cci_stream_event( kafka_producer_t kpt ,
+                                    stream_event_class sec ) : m_producer { kpt }
+{
+        assert( kpt );
+}
+
+
 
 
