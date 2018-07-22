7cd94180d924259eef5f1785642edae437dff5d6
chromatic-universe <chromaticuniverse79@gmail.com>
2018-07-21 20:41:07 -0400

    event_protocol_store

--- cci-cpp-stream/include/cci_stream_event.h
+++ cci-cpp-stream/include/cci_stream_event.h
@@ -7,15 +7,19 @@
 
 #include <cci_kafka_stream.h>
 #include <cci_curl_stream.h>
-
-
+#include <chromatic_protocol_store_t.h>
 
 
 namespace cpp_real_stream
 {
 
+                //forward declarations
+                template<typename T , typename T2>
+                class stream_protocol_store;
+
                 //aliases
                 using kafka_producer_t = cci_kafka_producer*;
+                using curl_stream_t = cci_curl_stream*;
 
 
                 //enumerations
@@ -37,7 +41,7 @@ namespace cpp_real_stream
 				};
 
 
-
+                //-------------------------------------------------------------------------------------------------------------------
                 class cci_stream_event
                 {
 
@@ -46,6 +50,7 @@ namespace cpp_real_stream
                                 //ctors
                                 cci_stream_event();
                                 explicit cci_stream_event(  kafka_producer_t kpt ,
+                                                            curl_stream_t cst ,
                                                             transport_mechanism tm = transport_mechanism::tm_http_post_to_queue ,
                                                             stream_event_class sec = stream_event_class::sec_internal );
 
@@ -66,6 +71,7 @@ namespace cpp_real_stream
 
                                 //attributes
                                 kafka_producer_t            m_producer;
+                                curl_stream_t               m_curl_stream;
 
 
                             protected :
@@ -81,14 +87,118 @@ namespace cpp_real_stream
 
                                 //accessors-inspectiors
                                 kafka_producer_t  producer() { return m_producer; }
+                                curl_stream_t  curl_strml() { return m_curl_stream; }
                                 stream_event_class event_class() const noexcept { return m_event_class; }
                                 transport_mechanism transport() const noexcept { return  m_transport; }
                                 //mutators
                                 void producer( kafka_producer_t kpt ) { m_producer = kpt; }
+                                void curl_strm( curl_stream_t cst ) { m_curl_stream = cst; }
                                 void event_class( const stream_event_class& ec ) { m_event_class = ec; }
                                 void transport( const transport_mechanism& tm ) { m_transport = tm; }
 
                 };
+                //-------------------------------------------------------------------------------------------------------------------
+                template<typename T , typename T2 = chromatic_protocol_store::cps_query>
+                class event_protocol_store : public chromatic_protocol_store::dispatcher_intf<T,T2>
+                {
+
+                    public :
+
+                        //ctors
+                        event_protocol_store( T* strm ) : m_ptr_stream{ strm } ,
+                                              m_result_stream( std::make_unique<std::ostringstream>() )
+                        {
+                        }
+
+
+                        explicit event_protocol_store( const std::string& context_param )
+                        {}
+
+                        //dtor
+                        virtual ~event_protocol_store()
+                        {}
+
+
+                        //no copy
+                        event_protocol_store( const event_protocol_store& ) = delete;
+                        //no assign
+                        const event_protocol_store& operator= ( const event_protocol_store&  ) = delete;
+
+
+                     private :
+
+                        //attributes
+                        std::string 				            m_context_param;
+                        T*					                    m_ptr_stream;
+                        std::unique_ptr<std::ostringstream>     m_result_stream;
+
+
+
+                     public :
+
+
+                        //accessors-inspectors }
+                        std::string context_param() const noexcept { return m_context_param; }
+                        //stream
+                        T* stream_ref()
+                        { return m_ptr_stream; }
+                        //out stream
+                        std::ostringstream* out_stream() { return m_result_stream.get(); }
+
+
+
+                        //service interface
+                        //------------------------------------------------------------------------------------------
+                        bool perform_naked_post_by_json_async( T2& q ,
+                                                               chromatic_protocol_store::naked_post_t npt  = nullptr ,
+                                                               unsigned future_wait = 1 );
+                        //------------------------------------------------------------------------------------------
+                        void prepare( T2& q )
+                        {}
+
+                };
+
+                //---------------------------------------------------------------------------------
+                template<typename T , typename T2>
+                bool event_protocol_store<T,T2>::perform_naked_post_by_json_async( T2& q ,
+                                                                                   chromatic_protocol_store::naked_post_t npt ,
+                                                                                   unsigned future_wait )
+                {
+                    //post with parameters
+                    bool b_ret { false };
+                    nlohmann::json j( q.simplex() );
+                    nlohmann::json u( { { "url" , q.url() } } );
+
+                    try
+                    {
+                        m_result_stream->str( "" );
+                        if( m_ptr_stream->results_by_naked_param( j ,
+                                                  u ,
+                                                  m_result_stream.get() ) )
+                        {
+                            if( npt )
+                            {
+
+                                nlohmann::json rj( m_result_stream.get()->str() );
+
+                                return npt( rj );
+
+                            }
+                            b_ret = true;
+                        }
+
+                    }
+                    catch( std::exception& e )
+                    {
+                        std::cerr << e.what()
+                              << "\n";
+                    }
+
+                    return b_ret;
+
+                }
+
+
 }
 
 
--- cci-cpp-stream/stream_event/src/cci_stream_event.cpp
+++ cci-cpp-stream/stream_event/src/cci_stream_event.cpp
@@ -17,12 +17,15 @@ cci_stream_event::cci_stream_event()
 
 //-----------------------------------------------------------------------------------------
 cci_stream_event::cci_stream_event( kafka_producer_t kpt ,
+                                    curl_stream_t cst ,
                                     transport_mechanism tm ,
                                     stream_event_class sec ) : m_producer { kpt } ,
+                                                               m_curl_stream { cst } ,
                                                                m_event_class { sec } ,
                                                                m_transport { tm }
 {
         assert( kpt );
+        assert( cst );
 }
 
 
