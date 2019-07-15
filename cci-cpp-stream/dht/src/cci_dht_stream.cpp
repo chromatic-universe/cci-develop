304355d62c1c67df5e0f0b0aa1e36c4715d662d2
chromatic-universe <chromaticuniverse79@gmail.com>
2019-07-15 03:06:34 -0400

    cci_dht_stream

--- cci-cpp-stream/dht/src/cci_dht_stream.cpp
+++ cci-cpp-stream/dht/src/cci_dht_stream.cpp
@@ -1,17 +1,49 @@
 //cci_dht_stream.cpp  chromatic universe 2019  william k. johnson
 
-
+//contrib
 #include <cci_dht_stream.h>
 
 
 using namespace cpp_real_stream;
 
+//immutable
+
+
+unsigned cci_dht_stream::dht_port = 13003UL;
+
 
 //-------------------------------------------------------------------------------------
-cci_dht_stream::cci_dht_stream()
+cci_dht_stream::cci_dht_stream() :  m_bootstrap { "nil" } ,
+                                    m_dht_node ( std::make_shared<dht_runner>() ) ,
+                                    m_port { cci_dht_stream::dht_port }
 {
-        //
 }
 
+//-------------------------------------------------------------------------------------
+cci_dht_stream::cci_dht_stream( const std::string& bootstrap ,
+                                unsigned port )
+try : m_bootstrap { bootstrap }  ,
+      m_dht_node( std::make_shared<dht_runner>() ) ,
+      m_port { port }
+{
+}
+catch( const dht::DhtException& dht_err )
+{
+        std::cerr << "...opendht library exception..."
+                  << dht_err.what()
+                  << "\n";
+
+}
+catch( const std::exception& err )
+{
+        std::cerr << err.what()
+                      << "\n";
+}
+
+//-------------------------------------------------------------------------------------
+cci_dht_stream::~cci_dht_stream()
+{
+        if( m_dht_node ) { m_dht_node->join(); }
+}
 
 
--- cci-cpp-stream/include/cci_dht_stream.h
+++ cci-cpp-stream/include/cci_dht_stream.h
@@ -13,6 +13,8 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <exception>
+
 //contrib
 #include <opendht.h>
 
@@ -23,6 +25,11 @@ namespace cpp_real_stream
                 //forward decalrations
 
 
+                //aliases
+                using dht_runner = dht::DhtRunner;
+                using dht_runner_t = dht_runner*;
+
+
                 //-------------------------------------------------------------------------------------------------------------------
                 class cci_dht_stream
                 {
@@ -30,12 +37,13 @@ namespace cpp_real_stream
                             public :
 
                                 //ctors
+                                explicit cci_dht_stream( const std::string& bootstrap =  "hil" ,
+                                                         unsigned port = dht_port );
                                 cci_dht_stream();
-                                //explicit cci_dht_stream();
 
 
                                 //dtor
-                                ~cci_dht_stream() = default;
+                                ~cci_dht_stream();
 
 
                                 //no copy
@@ -49,6 +57,9 @@ namespace cpp_real_stream
                             private :
 
                                 //attributes
+                                std::string                       m_bootstrap;
+                                std::shared_ptr<dht_runner>       m_dht_node;
+                                unsigned                          m_port;
 
 
                             protected :
@@ -64,8 +75,19 @@ namespace cpp_real_stream
 
 
                                 //accessors-inspectiors
+                                dht_runner_t node() { return m_dht_node.get(); }
+                                unsigned port() const noexcept { return m_port; }
+                                std::string bootstrap() const noexcept { return m_bootstrap; }
+                                //mutators
+                                void port( const unsigned& prt ) { m_port = prt; }
+                                void bootstrap( const std::string& strap ) { m_bootstrap = strap; }
+
 
                                 //services
+
+
+                            //immutable
+                            static unsigned dht_port;
 
                 };
 
